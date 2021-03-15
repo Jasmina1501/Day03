46a0a5b6fd8911d44e65812a1c7be58dd871329a
Jasmina Secic <jasmina.secic98@outlook.com>
2021-03-15 10:49:42 +0100

    js

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // This is a single line Javascript comment
+
+    /* document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    */ 
+}
